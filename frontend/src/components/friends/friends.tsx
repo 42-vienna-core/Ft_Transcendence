e5145e159c5e5e4b156b449c3f8b14fb258311f4
ibondarc <bondatchuk1989@gmail.com>
2026-07-02 11:38:53 +0200

    add an opportunity to delete friend

--- frontend/src/components/friends/friends.tsx
+++ frontend/src/components/friends/friends.tsx
@@ -3,15 +3,85 @@
 import { useEffect, useState } from 'react';
 import style from './friends.module.css'
 import { apiFetch } from '@/lib/api-client';
+import { UserRoundX } from 'lucide-react';
 
-interface Friends {
-    id: number;
-    name: string
-    avatar?: string | null;
+interface Friend {
+  id: number;
+  name: string;
+  avatar?: string | null;
+}
+
+interface FriendCardProps {
+    friend: Friend;
+    removeFriend: (id: number) => void;
+
+}
+
+interface ListOfFriendsProps {
+    friends: Friend[];
+    removeFriend: (id: number) => void;
+}
+
+function FriendCard({friend, removeFriend}: FriendCardProps) {
+    const {id, name, avatar} = friend;
+    const av = name && typeof name === "string" ? name.slice(0, 2) : "";
+
+    return (
+        <li key={id} className={`${style.fRow}`}>
+            <div>
+                { avatar ? (
+                    <img src={avatar ? avatar : ""} alt="avatar" />
+                ) : (
+                    <div className={`${style.av} bg-[var(--color-snake-1)] text-[var(--color-text-primary)] capitalize`}
+                        style={{ color: "var(--color-info-text)" }}
+                    >
+                        {av}
+                    </div>
+                )}
+            </div>
+            
+            <div className={style.main}>
+                <div className={style.who}>
+                    <span className={style.nm}>{name}</span>
+                </div>
+                <div className={style.statusLine}>
+                    <span className={`${style.dot} ${style.match}`}></span>
+                    <span>In public match · Room 47 · 3rd of 8</span>
+                    <span className={style.sep}>·</span>
+                    <span className={style.rating}>1920</span>
+                    <span className={`${style.delta} ${style.up}`}>▲42 wk</span>
+                </div>
+            </div>
+            <button 
+                className="flex items-center  px-2 py-1 bg-[var(--color-bg-muted)] text-red-600 rounded-full hover:bg-[var(--color-warning)] transition-colors"
+                onClick={() => removeFriend(id)}    
+                
+            >
+                
+                <UserRoundX className="w-4 h-4" />
+            </button>
+        </li>
+    )
+}
+
+function ListOfFriends({friends, removeFriend}: ListOfFriendsProps ) {
+    console.log("friends: ",friends);
+    return (
+        <ul className="flex flex-col gap-2">
+            {friends.length > 0 &&
+                (friends.map(friend => 
+                    <FriendCard 
+                        friend={friend}
+                        removeFriend={removeFriend}    
+                    />
+                ))
+            }
+        </ul>
+    )
 }
 
 export default function FriendsList () {
-    const [result, setResult] = useState<Friends[]>([]);
+    const [result, setResult] = useState<Friend[]>([]);
 
     useEffect(() => {
         const getListOfFriends = async () =>
@@ -21,46 +91,41 @@ export default function FriendsList () {
 
                 if (Array.isArray(res) && res.length != 0) {
                     setResult(res);
-                    console.log("FRIENDS LIST",res);
+                    console.log("FRIENDS LIST", res);
+                    return;
                 }
                 
                 setResult([]);
             } catch (error) {
-                
+                console.log("ERROR getting friends: ", error);
             }
         }
 
         getListOfFriends();
     }, []);
 
+    async function handeleFriendRemoving(id: number) {
+        if (id === 0) return;
+
+        try {
+            const url = `friends/${id}`
+            await apiFetch(url, {method: 'DELETE'});
+
+            setResult(prev => prev.filter(r => r.id !== id));
+        } catch (error) {
+            console.log("ERROR deleting a friend: ", error);
+        }
+    }
+
     return (
         <div className={style.col}>
-            <div className={`${style.fRow} ${style.starred}`}>
-              <span className={`${style.star} ${style.on}`} aria-label="Favorite">
-                <i className={`${style.ti} ${style.tiStar}`} aria-hidden="true"></i>
-              </span>
-              <div className={style.av}>MR</div>
-              <div className={style.main}>
-                <div className={style.who}>
-                  <span className={style.nm}>Mira</span>
-                  <span className={style.hd}>@mira_g</span>
-                </div>
-                <div className={style.statusLine}>
-                  <span className={`${style.dot} ${style.match}`}></span>
-                  <span>In public match · Room 47 · 3rd of 8</span>
-                  <span className={style.sep}>·</span>
-                  <span className={style.rating}>1920</span>
-                  <span className={`${style.delta} ${style.up}`}>▲42 wk</span>
-                </div>
-              </div>
-              <div className={style.actions}>
-                <button className={`${style.btn} ${style.primary}`}>
-                  <i className={`${style.ti} ${style.tiEye}`} aria-hidden="true"></i>{" "}
-                  Watch
-                </button>
-                <button className={style.btn}>Join</button>
-              </div>
-            </div>
+            <ListOfFriends 
+            friends={result}
+            removeFriend={handeleFriendRemoving}
+        />
+            {/* { result.length === 0 &&(
+                <p className="text-[var(--color-warning-text)] ">"No friends yet"</p>
+            )} */}
         </div>
     )
 }
