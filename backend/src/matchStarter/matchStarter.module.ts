7b7ed2f07990e40550227271bb651b8b76d12767
el-035 <elisa.fittant@gmail.com>
2026-08-29 13:33:58 +0200

    clean up expired and obsolete rooms

--- backend/src/matchStarter/matchStarter.module.ts
+++ backend/src/matchStarter/matchStarter.module.ts
@@ -5,10 +5,11 @@ import { GameRoomModule } from 'src/gameRoom/gameRoom.module';
 import { GameModule } from 'src/game/game.module';
 import { RedisModule } from 'src/redis/redis.module';
 import { FriendsModule } from 'src/friends/friends.module';
+import { RoomCleanUpService } from './roomCleanUp.service';
 
 @Module({
 	imports: [PrismaModule, GameRoomModule, GameModule, RedisModule, FriendsModule],
-	providers: [MatchStarter],
+	providers: [MatchStarter, RoomCleanUpService],
 	exports: [MatchStarter],
 })
 export class MatchModule {}
