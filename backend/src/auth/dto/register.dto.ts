d7c57d7e565d18d28cf9ec5fcadd19dbc4f1f16c
IgoryanDeltoro <bondatchuk1989@gmail.com>
2026-09-01 14:06:11 +0200

    add fivicon

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MaxLength , MinLength} from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class RegisterRequest {
 
@@ -18,5 +18,8 @@ export class RegisterRequest {
     @IsString()
     @MinLength(8)
     @MaxLength(128)
+    @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).+$/, {
+        message: 'Weak password',
+    })
     password!: string;
 }
